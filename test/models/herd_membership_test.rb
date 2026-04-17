318df4cb563c06b109bb59b2f0093254806a957b
Gaurav Sharma <gaurav@engagedly.com>
2026-04-17 23:11:49 +0530

    Implement user authentication system with password reset functionality
    - Added `User`, `Session`, and `PasswordsController` models and controllers to handle user authentication and password resets.
    - Introduced `Authentication` concern for managing user sessions and access control.
    - Created views for password reset and session management.
    - Updated `Gemfile` to include `bcrypt` for password hashing.
    - Added tests for password and session functionalities.
    - Implemented mailer for sending password reset instructions.

---
+++ test/models/herd_membership_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class HerdMembershipTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/squad_membership_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SquadMembershipTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
