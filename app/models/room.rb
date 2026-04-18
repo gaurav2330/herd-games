c77966f8f400c47ead1f98e92d28cb81b9874365
Gaurav Sharma <gaurav@engagedly.com>
2026-04-18 21:32:53 +0530

    Update room and squad models to enhance associations
    - Made the `squad` association in the `Room` model optional, allowing for more flexible room assignments.
    - Added an `admin` association to the `Squad` model to improve admin management capabilities.

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
   belongs_to :game
-  belongs_to :squad
+  belongs_to :squad, optional: true
   belongs_to :admin, class_name: "User", foreign_key: "admin_id"
   has_many :room_memberships, dependent: :destroy
   has_many :users, through: :room_memberships
--- app/models/squad.rb
+++ app/models/squad.rb
@@ -2,4 +2,5 @@ class Squad < ApplicationRecord
   has_many :squad_memberships, dependent: :destroy
   has_many :users, through: :squad_memberships
   has_many :rooms, dependent: :destroy
+  belongs_to :admin, class_name: "User", foreign_key: "admin_id"
 end
