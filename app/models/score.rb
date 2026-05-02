518c13402eed58cedb5874599fb9e629cfbe7f14
Gaurav Sharma <sgrb26@gmail.com>
2026-05-02 18:57:18 +0530

    Add room score update logic in Score model and update scoreboard view
    - Introduced after_create callback in Score model to update room scores when a round score is created.
    - Added round_score? method to differentiate between scoreable types.
    - Refactored scoreboard view to display room scores instead of round scores for improved accuracy.

--- app/models/score.rb
+++ app/models/score.rb
@@ -3,6 +3,7 @@ class Score < ApplicationRecord
   belongs_to :scoreable, polymorphic: true
 
   after_create :update_round_score, if: :turn_score?
+  after_create :update_room_score, if: :round_score?
 
   private
 
@@ -10,10 +11,21 @@ class Score < ApplicationRecord
     scoreable_type == "Turn"
   end
 
+  def round_score?
+    scoreable_type == "Round"
+  end
+
   def update_round_score
     round = scoreable.round
     round_score = Score.find_or_initialize_by(user: user, scoreable: round)
     round_score.points = Score.where(user: user, scoreable: round.turns).sum(:points)
     round_score.save!
+  end
+
+  def update_room_score
+    room = scoreable.room
+    room_score = Score.find_or_initialize_by(user: user, scoreable: room)
+    room_score.points = Score.where(user: user, scoreable: room.rounds).sum(:points)
+    room_score.save!
   end
 end
